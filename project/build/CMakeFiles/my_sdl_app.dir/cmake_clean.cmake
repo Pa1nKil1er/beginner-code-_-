d6ecf08030a0c99c025ca453b655f706790b57ff
Pa1nKil1er <15105047833@163.com>
2026-06-03 22:53:25 +0800

    大一上c语言课设—图形化界面实现的电话簿
    大概3天完成，在ai的帮助下实现了
    1.引入第三方库sdl2，实现了图形化的界面
    2.利用cmake将文件打包
    但是由于可以直接跟着ai的命令做，所以大多数时间都画在配置环境，调试，将各部分代码合在一起上
    所以感觉对sdl2和cmake并没有怎么掌握。
    不过并非完全没有收获：
    1.初步认识到应用型的代码类型，而不只是局限于算法题那种简洁求快的代码
    2.有一点点关于代码管理的感觉（虽然还很浅orz

---
+++ project/build/CMakeFiles/my_sdl_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_sdl_app.dir/main.c.obj"
+  "CMakeFiles/my_sdl_app.dir/main.c.obj.d"
+  "libmy_sdl_app.dll.a"
+  "my_sdl_app.exe"
+  "my_sdl_app.exe.manifest"
+  "my_sdl_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_sdl_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
